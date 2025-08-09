145bcf7f32c44d1febc24a2486a8df6d78d48132
pranaaavj <jpvelloor@gmail.com>
2025-08-09 16:27:46 +0530

    Changes made

---
+++ packages/proto-contracts/dist/index.js
@@ -0,0 +1,2 @@
+export { CacheServiceClient, CacheServiceService, GetRequest, GetResponse, InvalidateRequest, InvalidateResponse, Product, SetRequest, SetResponse, } from './generated/cache_service.js';
+export { CreateProductRequest, DeleteProductRequest, DeleteResponse, GetProductRequest, ProductServiceClient, ProductServiceService, UpdateProductRequest, } from './generated/product.js';
